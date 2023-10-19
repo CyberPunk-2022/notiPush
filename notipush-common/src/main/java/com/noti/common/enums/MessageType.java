e67a8bd48f16cbab41abc40f07f97a2fc6d83561
杨奇翰 <1215937291@qq.com>
2023-10-19 21:37:09 +0800

    kafka接入

---
+++ notipush-common/src/main/java/com/noti/common/enums/MessageType.java
@@ -0,0 +1,53 @@
+package com.noti.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.Getter;
+
+/**
+ * 发送的消息类型
+ */
+@Getter
+@ToString
+@AllArgsConstructor
+public enum MessageType {
+
+    NOTICE(10,"通知类消息","notice"),
+    MARKETING(20,"营销类消息","marketing"),
+    AUTH_CODE(30,"验证码消息","auth_code")
+    ;
+
+    /**
+     * 编码值
+     */
+    private Integer code;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+
+    /**
+     * 英文标识
+     */
+    private String codeEn;
+
+
+    /**
+     * 通过code获取enum
+     * @param code
+     * @return
+     */
+    public static MessageType getEnumByCode(Integer code) {
+        MessageType[] values = values();
+        for (MessageType value : values) {
+            if (value.getCode().equals(code)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ notipush-handler/src/main/java/com/noti/handler/receiver/kafka/Receiver.java
@@ -0,0 +1,61 @@
+
+package com.noti.handler.receiver.kafka;
+
+import cn.hutool.core.collection.CollUtil;
+import com.alibaba.fastjson.JSON;
+import com.noti.handler.utils.GroupIdMappingUtils;
+import com.noti.support.constans.MessageQueuePipeline;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Scope;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+import com.noti.common.domain.TaskInfo;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 消费MQ的消息
+ */
+@Slf4j
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@ConditionalOnProperty(name = "austin.mq.pipeline", havingValue = MessageQueuePipeline.KAFKA)
+public class Receiver {
+    /**
+     * 发送消息
+     *
+     * @param consumerRecord
+     * @param topicGroupId
+     */
+    @KafkaListener(topics = "#{'${austin.business.topic.name}'}", containerFactory = "filterContainerFactory")
+    public void consumer(ConsumerRecord<?, String> consumerRecord, @Header(KafkaHeaders.GROUP_ID) String topicGroupId) {
+        Optional<String> kafkaMessage = Optional.ofNullable(consumerRecord.value());
+        if (kafkaMessage.isPresent()) {
+
+            List<TaskInfo> taskInfoLists = JSON.parseArray(kafkaMessage.get(), TaskInfo.class);
+            String messageGroupId = GroupIdMappingUtils.getGroupIdByTaskInfo(CollUtil.getFirst(taskInfoLists.iterator()));
+            /**
+             * 每个消费者组 只消费 他们自身关心的消息
+             */
+            if (topicGroupId.equals(messageGroupId)) {
+                log.info("groupId:{},params:{}", messageGroupId, JSON.toJSONString(taskInfoLists));
+            }
+        }
+    }
+
+    /**
+     * 撤回消息
+     *
+     * @param consumerRecord
+     */
+    @KafkaListener(topics = "#{'${austin.business.recall.topic.name}'}", groupId = "#{'${austin.business.recall.group.name}'}", containerFactory = "filterContainerFactory")
+    public void recall(ConsumerRecord<?, String> consumerRecord) {
+
+    }
+}

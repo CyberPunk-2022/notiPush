eecc9d82b56171aa4656db9a5a6384f62ac5ef26
杨奇翰 <1215937291@qq.com>
2023-10-20 12:52:24 +0800

    实现去重逻辑

---
+++ notipush-handler/src/main/java/com/noti/handler/duplication/build/AbstractDeduplicationBuilder.java
@@ -0,0 +1,34 @@
+package com.noti.handler.duplication;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.Objects;
+
+public abstract class AbstractDeduplicationBuilder implements Builder {
+
+    protected Integer deduplicationType;
+
+    @Autowired
+    private DeduplicationHolder deduplicationHolder;
+
+    @PostConstruct
+    public void init() {
+        deduplicationHolder.putBuilder(deduplicationType, this);
+    }
+
+    public DeduplicationParam getParamsFromConfig(Integer key, String duplicationConfig, TaskInfo taskInfo) {
+        JSONObject object = JSONObject.parseObject(duplicationConfig);
+        if (Objects.isNull(object)) {
+            return null;
+        }
+        DeduplicationParam deduplicationParam = JSONObject.parseObject(object.getString(DEDUPLICATION_CONFIG_PRE + key), DeduplicationParam.class);
+        if (Objects.isNull(deduplicationParam)) {
+            return null;
+        }
+        deduplicationParam.setTaskInfo(taskInfo);
+        return deduplicationParam;
+    }
+
+}
